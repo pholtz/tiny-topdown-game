a83c1348edac3a28fd0f64b688e37adf5429d7bf
Paul Holtz <paul.holtz@qvc.com>
2020-12-23 17:16:30 -0500

    break everything and start piston -> ggez conversion

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
 extern crate find_folder;
-extern crate glutin_window;
-extern crate graphics;
-extern crate opengl_graphics;
-extern crate piston;
 extern crate tiled;
 
 use std::{collections::{BTreeMap, HashMap}};
+use std::time::{Duration, Instant};
+use std::path;
+use std::env;
 
-use piston::window::WindowSettings;
-use piston_window::*;
 use specs::prelude::*;
 use specs_derive::Component;
+
+use ggez::event::{KeyCode, KeyMods};
+use ggez::{graphics, Context, ContextBuilder, GameResult, graphics::Rect};
+use ggez::event::{self, EventHandler};
+use ggez::nalgebra as na;
 
 mod map;
 pub use map::*;
@@ -30,6 +32,37 @@ enum Direction {
     Left,
     Right,
 }
+
+impl Direction {
+    /// We create a helper function that will allow us to easily get the inverse
+    /// of a `Direction` which we can use later to check if the player should be
+    /// able to move the snake in a certain direction.
+    pub fn inverse(&self) -> Self {
+        match *self {
+            Direction::Up => Direction::Down,
+            Direction::Down => Direction::Up,
+            Direction::Left => Direction::Right,
+            Direction::Right => Direction::Left,
+        }
+    }
+
+    /// We also create a helper function that will let us convert between a
+    /// `ggez` `Keycode` and the `Direction` that it represents. Of course,
+    /// not every keycode represents a direction, so we return `None` if this
+    /// is the case.
+    pub fn from_keycode(key: KeyCode) -> Option<Direction> {
+        match key {
+            KeyCode::Up | KeyCode::W => Some(Direction::Up),
+            KeyCode::Down | KeyCode::S => Some(Direction::Down),
+            KeyCode::Left | KeyCode::A => Some(Direction::Left),
+            KeyCode::Right | KeyCode::D => Some(Direction::Right),
+            _ => None,
+        }
+    }
+}
+
+type Point2 = na::Point2<f32>;
+type Vector2 = na::Vector2<f32>;
 
 struct State {
     ecs: World,
@@ -56,7 +89,242 @@ pub const TL_PX: i32 = 32;
 pub const WIDTH_TL: i32 = WIDTH_PX / TL_PX;
 pub const HEIGHT_TL: i32 = HEIGHT_PX / TL_PX;
 
-fn main() {
+
+const UPDATES_PER_SECOND: f32 = 60.0;
+const MILLIS_PER_UPDATE: u64 = (1.0 / UPDATES_PER_SECOND * 1000.0) as u64;
+
+
+fn main() -> GameResult {
+    // We add the CARGO_MANIFEST_DIR/resources to the resource paths
+    // so that ggez will look in our cargo project directory for files.
+    let resource_dir = if let Ok(manifest_dir) = env::var("CARGO_MANIFEST_DIR") {
+        let mut path = path::PathBuf::from(manifest_dir);
+        path.push("assets");
+        path
+    } else {
+        path::PathBuf::from("./assets")
+    };
+
+    // Make a Context.
+    let (ctx, event_loop) = &mut ContextBuilder::new("Tiny Topdown Game", "Paul Holtz")
+        .window_setup(ggez::conf::WindowSetup::default().title("Tiny Topdown Game"))
+        .window_mode(ggez::conf::WindowMode::default().dimensions(WIDTH_PX as f32, HEIGHT_PX as f32))
+        .add_resource_path(resource_dir)
+        .build()
+		.expect("aieee, could not create ggez context!");
+
+    // Create an instance of your event handler.
+    // Usually, you should provide it with the Context object to
+    // use when setting your game up.
+    let state = &mut GameState::new(ctx);
+
+    // Run!
+    event::run(ctx, event_loop, state)
+}
+
+struct GameState {
+    last_update: Instant,
+    ecs: World,
+    tilesheet: graphics::Image
+}
+
+impl GameState {
+    pub fn new(ctx: &mut Context) -> GameState {
+        // Load/create resources such as images here.
+        let assets = find_folder::Search::ParentsThenKids(3, 3)
+            .for_folder("assets")
+            .unwrap();
+
+        let mut world = World::new();
+        world.register::<Position>();
+        world.register::<Renderable>();
+        world.register::<Player>();
+
+        world.create_entity()
+            .with(Position { x: 0, y: 0 })
+            .with(Renderable {})
+            .with(Player {
+                direction: Direction::Down
+            })
+            .build();
+
+        let mut textures_by_player_direction = HashMap::new();
+        textures_by_player_direction.insert(Direction::Down, graphics::Image::new(ctx, "/basic_guy.png").expect("could not load image"));
+        textures_by_player_direction.insert(Direction::Up, graphics::Image::new(ctx, "/basic_guy_up.png").expect("could not load image"));
+        textures_by_player_direction.insert(Direction::Left, graphics::Image::new(ctx, "/basic_guy_left.png").expect("could not load image"));
+        textures_by_player_direction.insert(Direction::Right, graphics::Image::new(ctx, "/basic_guy_right.png").expect("could not load image"));
+        world.insert(textures_by_player_direction);
+
+        let font = graphics::Font::new(ctx, "/FiraSans-Regular.ttf");
+        let tileset_image = graphics::Image::new(ctx, "/grass_tileset.png").expect("could not load image");
+
+        let (map, tilesheet) = load_basic_map_tmx();
+        world.insert(map);
+        world.insert(tilesheet);
+        // world.insert(tileset_image);
+
+        GameState {
+            last_update: Instant::now(),
+            ecs: world,
+            tilesheet: tileset_image,
+        }
+    }
+}
+
+impl EventHandler for GameState {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        // Ensure that at least a full tick has elapsed before updating
+        if Instant::now() - self.last_update < Duration::from_millis(MILLIS_PER_UPDATE) {
+            return Ok(());
+        }
+
+
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        // First we clear the screen to a nice (well, maybe pretty glaring ;)) green
+        graphics::clear(ctx, [0.0, 1.0, 0.0, 1.0].into());
+        
+        // Then we tell the snake and the food to draw themselves
+        // self.snake.draw(ctx)?;
+        // self.food.draw(ctx)?;
+
+        let player = self.ecs.read_storage::<Player>();
+        let positions = self.ecs.read_storage::<Position>();
+        let (_player, pos) = (&player, &positions).join().next().expect("No player and position entities found");
+
+        let viewport = calculate_viewport((pos.x, pos.y));
+
+        let viewport_tiles = generate_viewport_tiles(viewport);
+
+        // Render viewport tiles in place on screen
+        // Render each tile using the given pixel positions
+        let map = self.ecs.fetch::<BTreeMap<(i32, i32), MapTile>>();
+        let tilesheet = self.ecs.fetch::<TileSheet>();
+        // let tilesheet_image = self.ecs.fetch::<graphics::Image>();
+
+        for (tile_x, tile_y, _view_x, _view_y, screen_x, screen_y) in viewport_tiles.iter() {
+
+            // let tile_transform = c.transform.trans(*screen_x as f64, *screen_y as f64);
+
+            // Retrieve the appropriate tile type from the map using the tile coordinates
+            let map_tile = match map.get(&(*tile_x, *tile_y)) {
+                Some(map_tile) => map_tile,
+                None => &MapTile {
+                    tile_id: 10,
+                    tile_type: TileType::Missing
+                }
+            };
+            
+            // Calculate tilesheet subregion containing the sprite matching the desired gid
+            
+            // This is the local index of the desired tile on the tilesheet (0 through tiles - 1)
+            // This also accounts for the fact that Tiled indexes start at 1, but we use 0 indexed offsets
+            let tile_index = map_tile.tile_id - tilesheet.first_tile_id;
+            let horizontal_index = tile_index % 10;
+            let vertical_index = tile_index / 10;
+
+            // Presumably, the margin from tiled means the space between the borders and the edge tiles
+            // As such, we will always add margin, but width and spacing will be dependent on our index
+            let horizontal_offset = tilesheet.margin + (horizontal_index * tilesheet.tile_width) + (horizontal_index * tilesheet.spacing);
+
+            // Vertical offset is a bit trickier, since tiles are only stacked once they exceed horizontal space
+            // To calculate this elegantly we can modulo using the number of tiles per row
+            let vertical_offset = tilesheet.margin + (vertical_index * tilesheet.tile_height) + (vertical_index * tilesheet.spacing);
+
+            let tile_rectangle = [
+                horizontal_offset as f32,
+                vertical_offset as f32,
+                tilesheet.tile_width as f32,
+                tilesheet.tile_height as f32
+            ];
+
+            // Image::new().src_rect(tile_rectangle).draw(&*tilesheet_image, &c.draw_state, tile_transform, g);
+            let drawparams = graphics::DrawParam::new()
+                // .src(Rect::new(tile_rectangle[0], tile_rectangle[1], tile_rectangle[2], tile_rectangle[3]))
+                .src(Rect::new(2 as f32, 2 as f32, 34 as f32, 34 as f32))
+                .dest(Point2::new(*screen_x as f32, *screen_y as f32))
+                .offset(Point2::new(0.5, 0.5));
+            // graphics::draw(ctx, &self.tilesheet, drawparams)?;
+        }
+
+        // TODO: .src() rectangles represent a fraction of the provided image
+        // We can use this to render individual sprites from a spritesheet,
+        // similar to piston's .src_rect(). However, ggez expects them to be
+        // specified as fractions, not pixels, so we should adjust the tilesets
+        // going into tiled to stop using margin and spacing, and have easily
+        // divisible numbers of rows and columns (say, 10x10).
+        graphics::draw(ctx, &self.tilesheet, graphics::DrawParam::new()
+            .src(Rect::new(0 as f32, 0 as f32, 0.1 as f32, 0.1333 as f32))
+            .dest(Point2::new(0 as f32, 0 as f32)))?;
+
+        // Render player in place on screen
+        // This is easy now since we will always just render the player in the middle of the screen
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+        let players = self.ecs.read_storage::<Player>();
+        let textures_by_player_direction = self.ecs.fetch::<HashMap<Direction, graphics::Image>>();
+        for (_pos, _render, player) in (&positions, &renderables, &players).join() {
+            let drawparams = graphics::DrawParam::new()
+                .dest(Point2::new((WIDTH_PX / 2) as f32, (HEIGHT_PX / 2) as f32))
+                .offset(Point2::new(0.5, 0.5));
+            graphics::draw(ctx,
+                textures_by_player_direction.get(&player.direction).expect("could not source player texture"),
+                drawparams)?;
+        }
+
+        // Finally we call graphics::present to cycle the gpu's framebuffer and display
+        // the new frame we just drew and then yield the thread until the next update.
+        graphics::present(ctx)?;
+        ggez::timer::yield_now();
+        Ok(())
+    }
+
+    /// key_down_event gets fired when a key gets pressed.
+    fn key_down_event(
+        &mut self,
+        ctx: &mut Context,
+        keycode: KeyCode,
+        _keymod: KeyMods,
+        _repeat: bool,
+    ) {
+        // Here we attempt to convert the Keycode into a Direction using the helper
+        // we defined earlier.
+        if let Some(dir) = Direction::from_keycode(keycode) {
+            try_move_player(dir, &self.ecs);
+        }
+
+        match keycode {
+            KeyCode::Escape => event::quit(ctx),
+            _ => (), // Do nothing
+        }
+    }
+}
+
+fn try_move_player(direction: Direction, ecs: &World) {
+    let delta = match direction {
+        Direction::Up => (0, -1 * TL_PX),
+        Direction::Left => (-1 * TL_PX, 0),
+        Direction::Down => (0, TL_PX),
+        Direction::Right => (TL_PX, 0),
+    };
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+    for (player, pos) in (&mut players, &mut positions).join() {
+        player.direction = direction;
+        pos.x += delta.0;
+        pos.y += delta.1;
+    }
+}
+
+
+
+
+
+
+/*
+fn main2() {
     let opengl = OpenGL::V3_2;
 
     let mut window: PistonWindow = WindowSettings::new("Tiny Topdown Game", [WIDTH_PX as u32, HEIGHT_PX as u32])
@@ -272,3 +540,4 @@ fn start_transition(event: &Event) -> RootState {
         _ => RootState::StartMenu,
     }
 }
+*/
